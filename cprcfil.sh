63f2076b0f3423e97996800adef1b5c813029406
shivanshu21 <shivanshu.goswami@ril.com>
2015-11-04 16:32:40 +0530

    automatic file copying

---
+++ cprcfil.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+sudo cp rcfiles/.bashrc ~
+sudo cp rcfiles/.vimrc ~
+sudo cp rcfiles/.screenrc ~
+sudo cp rcfiles/.nanorc ~
+sudo cp rcfiles/.dir_colors ~
+sudo cp wenv.sh ~
+sudo cp minions ~
+sudo cp vpn.sh ~
+sudo cp vpn_conf ~
+sudo cp alive.sh ~
+sudo chmod 755 ~/minions
+sudo chmod 755 ~/wenv.sh
