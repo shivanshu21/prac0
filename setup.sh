429b9990e62719140bcfd3b59ad01c08f7642ad7
shivanshu21 <shivanshu.goswami@ril.com>
2015-12-08 14:08:49 +0000

    proper vagrant setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,12 @@
-sudo apt-get -y install git
-sudo apt-get -y install screen
-sudo apt-get -y install vim
+cp /vagrant/apt.conf /etc/apt
+sudo -i
+apt-get -y update
+apt-get -y install git
+apt-get -y install screen
+apt-get -y install vim
+mkdir /root/gitcode
+cd /root/gitcode
+git clone https://github.com/shivanshu21/prac0.git
+cd prac0
+chmod 755 cprcfil.sh
+./cprcfil.sh
