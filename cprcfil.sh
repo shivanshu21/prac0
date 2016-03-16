4dd9ed7c39034147478800ec62f39d9c43dddc61
shivanshu21 <shivanshu.goswami@ril.com>
2016-03-16 21:29:18 +0530

    Copying tailcomm

--- cprcfil.sh
+++ cprcfil.sh
@@ -9,5 +9,7 @@ sudo cp minions ~
 sudo cp vpn.sh ~
 sudo cp vpn_conf ~
 sudo cp alive.sh ~
+sudo cp dev_vagrant/tailcomm ~
 sudo chmod 755 ~/minions
 sudo chmod 755 ~/wenv.sh
+sudo chmod 755 ~/tailcomm
