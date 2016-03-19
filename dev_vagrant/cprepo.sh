a9e947e82cb22233b6f80fd100c3b9c274c0134a
shivanshu21 <shivanshu.goswami@ril.com>
2016-03-19 13:56:37 +0530

    Vagrant workspace to have inherent repo of scripts now

---
+++ dev_vagrant/cprepo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+echo "Copying from workspace to repo"
+cp -v ~/workspace/* ~/workspace/prac0/dev_vagrant
---
+++ dev_vagrant/cpws.sh
@@ -0,0 +1,3 @@
+#! /bin/bash
+echo "Copying from repo to workspace..."
+cp -v ~/workspace/prac0/dev_vagrant/* ~/workspace
