d03be7271741d7840d66c985110913706b43a48d
Shivanshu Goswami <shivanshu21@gmail.com>
2016-05-02 18:57:14 +0530

    Final tuning for java tokens

---
+++ dev_vagrant/javaTokens/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd jdk1.8.0_77/bin
+./java -jar ../../JcsIamKeystoneTest.jar CreateToken C iam.ind-west-1.staging.jiocloudservices.com "" Y 544684726711 dss_console_test01 Reliance__123 239d61b4819b40b7bb37b2a5db0cfa7f 5355aff590b841e48261ee44e8f0ebb5 511054669218 dss_test_0000 Reliance_123
