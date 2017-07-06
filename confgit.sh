d7e4ab0d92072598775b19d534bf0c3df908da81
shivanshu21 <shivanshug@vmware.com>
2017-07-06 12:58:32 -0700

    Bashrc updates and some useful files

---
+++ confgit.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+git config --global user.name "shivanshu21"
+git config --global user.email "shivanshug@vmware.com"
+git remote add upstream https://github.com/vmware/docker-volume-vsphere.git
+git remote add miao https://github.com/luomiao/docker-volume-vsphere.git
+
